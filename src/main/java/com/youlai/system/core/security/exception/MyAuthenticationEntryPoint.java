3cb99e0d2d14b4ff5dbd71ea864afdcd0c3ebc6a
zc <43929367+zhangchuan11@users.noreply.github.com>
2023-12-29 12:22:05 +0800

    fix(添加登录失败信息): 添加登录失败信息

--- src/main/java/com/youlai/system/core/security/exception/MyAuthenticationEntryPoint.java
+++ src/main/java/com/youlai/system/core/security/exception/MyAuthenticationEntryPoint.java
@@ -2,11 +2,11 @@ package com.youlai.system.core.security.exception;
 
 import com.youlai.system.common.result.ResultCode;
 import com.youlai.system.common.util.ResponseUtils;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -20,14 +20,20 @@ import java.io.IOException;
 @Component
 public class MyAuthenticationEntryPoint implements AuthenticationEntryPoint {
         @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         int status = response.getStatus();
         if (status == HttpServletResponse.SC_NOT_FOUND) {
             // 资源不存在
             ResponseUtils.writeErrMsg(response, ResultCode.RESOURCE_NOT_FOUND);
         } else {
-            // 未认证或者token过期
-            ResponseUtils.writeErrMsg(response, ResultCode.TOKEN_INVALID);
+          if(authException instanceof UsernameNotFoundException){
+              ResponseUtils.writeErrMsg(response, ResultCode.USER_NOT_EXIST);
+          }else if(authException instanceof BadCredentialsException){
+              ResponseUtils.writeErrMsg(response, ResultCode.USERNAME_OR_PASSWORD_ERROR);
+          }else {
+              // 未认证或者token过期
+              ResponseUtils.writeErrMsg(response, ResultCode.TOKEN_INVALID);
+          }
         }
     }
 }
